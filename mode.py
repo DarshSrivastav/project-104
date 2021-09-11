c7d59dc39f9cdf9978247a72fd4389fe97cd49bd
DarshSrivastav <72658598+DarshSrivastav@users.noreply.github.com>
2021-09-11 09:26:51 +0530

    Add files via upload

---
+++ mode.py
@@ -0,0 +1,10 @@
+import csv
+with open('height-weight.csv',newline='') as f:
+    reader=csv.reader(f)
+    file_data=list(reader)
+
+file_data.pop(0)
+new_data=[]
+for i in range(len(file_data)):
+    n_num=file_data[i][1]
+    new_data.append(float(n_num))
